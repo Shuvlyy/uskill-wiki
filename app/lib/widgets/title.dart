d162828b8f2c5123749f106fff93df78890dfaf4
Lysandre B. <lysandre.boursette@epitech.eu>
2026-06-26 22:36:53 +0200

    feat(app>widget>title): Added a center-styled title.

--- app/lib/widgets/title.dart
+++ app/lib/widgets/title.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
+enum TitleDecorationAlignment { normal, center }
+
 class Title extends StatelessWidget {
-  const Title({super.key, required this.title});
+  const Title({
+    super.key,
+    required this.title,
+    this.decorationAlignment = .normal,
+  });
 
   final String title;
+  final TitleDecorationAlignment decorationAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: .start,
+      crossAxisAlignment: decorationAlignment == .center ? .center : .start,
       children: [
         Text(title, style: Theme.of(context).textTheme.headlineMedium),
         Container(width: 50, height: 5, color: Theme.of(context).primaryColor),
