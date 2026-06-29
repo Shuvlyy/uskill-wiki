20cb23009fbb572943abe4469eb6600b35dd185e
Lysandre B. <lysandre.boursette@epitech.eu>
2026-06-29 14:00:58 +0200

    feat(app>screen>widget_test): Now uses main page layout.

--- app/lib/screens/widget_test.dart
+++ app/lib/screens/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:app/layouts/main_page_layout.dart';
 import 'package:app/widgets/button.dart';
 import 'package:app/widgets/dot_stepper.dart';
 import 'package:app/widgets/icon_button.dart';
@@ -24,123 +25,96 @@ class _WidgetTestState extends State<WidgetTest> {
   int _currentDotStepperIndex = 1;
   final _dotStepperAmount = 6;
 
-  int _selectedNavbarIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    final navbarItems = [
-      NavbarItem(
-        label: 'Ressources',
-        isSelected: _selectedNavbarIndex == 0,
-        onTap: () => setState(() => _selectedNavbarIndex = 0),
-      ),
-      NavbarItem(
-        label: 'Dépôt de ressource',
-        isSelected: _selectedNavbarIndex == 1,
-        onTap: () => setState(() => _selectedNavbarIndex = 1),
-      ),
-      NavbarItem(
-        label: 'À propos',
-        isSelected: _selectedNavbarIndex == 2,
-        onTap: () => setState(() => _selectedNavbarIndex = 2),
-      ),
-    ];
-
-    return Scaffold(
-      appBar: NavbarWrapper(items: navbarItems),
-      drawer: NavbarDrawer(items: navbarItems),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: .start,
+    return MainPageLayout(
+      body: Column(
+        crossAxisAlignment: .start,
+        spacing: 20,
+        children: [
+          Wrap(
+            runSpacing: 20,
             spacing: 20,
             children: [
-              Wrap(
-                runSpacing: 20,
-                spacing: 20,
-                children: [
-                  Logo.univBlack(height: 92),
-                  Container(
-                    color: Colors.black,
-                    child: Logo.univWhite(height: 92),
-                  ),
-                ],
-              ),
-              Title(title: 'Normal title'),
-              Title(title: 'Centered title', decorationAlignment: .center),
-              Button.primary(text: 'Primary', onPressed: () {}),
-              Button.secondary(text: 'Secondary', onPressed: () {}),
-              Button.inverted(text: 'Inverted', onPressed: () {}),
-              Button.outlined(text: 'Outlined', onPressed: () {}),
-              PrimaryIconButton(
-                icon: Icons.arrow_forward_ios,
-                onPressed: () {},
-              ),
-              TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(hintText: 'Text form field'),
-              ),
-              DropdownMenu(
-                hintText: 'Dropdown form field',
-
-                expandedInsets: EdgeInsets.zero,
-                requestFocusOnTap: false,
-
-                dropdownMenuEntries: [
-                  DropdownMenuEntry<String>(value: 'item1', label: 'Item 1'),
-                  DropdownMenuEntry<String>(value: 'item2', label: 'Item 2'),
-                  DropdownMenuEntry<String>(value: 'item3', label: 'Item 3'),
-                  DropdownMenuEntry<String>(value: 'item4', label: 'Item 4'),
-                  DropdownMenuEntry<String>(value: 'item5', label: 'Item 5'),
-                ],
-                onSelected: (value) {},
-              ),
-              OptionSlider(
-                steps: _levels,
-                selectedIndex: _currentLevelIndex,
-                onChanged: (int newIndex) {
-                  setState(() {
-                    _currentLevelIndex = newIndex;
-                  });
-                },
-              ),
-              Row(
-                mainAxisSize: .min,
-                children: [
-                  DotStepper(
-                    amount: _dotStepperAmount,
-                    index: _currentDotStepperIndex,
-                  ),
-                  const Gap(10),
-                  IconButton(
-                    icon: Icon(Icons.remove),
-                    onPressed: () {
-                      setState(() {
-                        if (_currentDotStepperIndex == 0) {
-                          return;
-                        }
-                        _currentDotStepperIndex--;
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      setState(() {
-                        if (_currentDotStepperIndex == _dotStepperAmount) {
-                          return;
-                        }
-                        _currentDotStepperIndex++;
-                      });
-                    },
-                  ),
-                ],
+              Logo.univBlack(height: 92),
+              Container(
+                color: Colors.black,
+                child: Logo.univWhite(height: 92),
               ),
             ],
           ),
-        ),
-      ),
+          Title(title: 'Normal title'),
+          Title(title: 'Centered title', decorationAlignment: .center),
+          Button.primary(text: 'Primary', onPressed: () {}),
+          Button.secondary(text: 'Secondary', onPressed: () {}),
+          Button.inverted(text: 'Inverted', onPressed: () {}),
+          Button.outlined(text: 'Outlined', onPressed: () {}),
+          PrimaryIconButton(
+            icon: Icons.arrow_forward_ios,
+            onPressed: () {},
+          ),
+          TextFormField(
+            controller: _controller,
+            decoration: InputDecoration(hintText: 'Text form field'),
+          ),
+          DropdownMenu(
+            hintText: 'Dropdown form field',
+
+            expandedInsets: EdgeInsets.zero,
+            requestFocusOnTap: false,
+
+            dropdownMenuEntries: [
+              DropdownMenuEntry<String>(value: 'item1', label: 'Item 1'),
+              DropdownMenuEntry<String>(value: 'item2', label: 'Item 2'),
+              DropdownMenuEntry<String>(value: 'item3', label: 'Item 3'),
+              DropdownMenuEntry<String>(value: 'item4', label: 'Item 4'),
+              DropdownMenuEntry<String>(value: 'item5', label: 'Item 5'),
+            ],
+            onSelected: (value) {},
+          ),
+          OptionSlider(
+            steps: _levels,
+            selectedIndex: _currentLevelIndex,
+            onChanged: (int newIndex) {
+              setState(() {
+                _currentLevelIndex = newIndex;
+              });
+            },
+          ),
+          Row(
+            mainAxisSize: .min,
+            children: [
+              DotStepper(
+                amount: _dotStepperAmount,
+                index: _currentDotStepperIndex,
+              ),
+              const Gap(10),
+              IconButton(
+                icon: Icon(Icons.remove),
+                onPressed: () {
+                  setState(() {
+                    if (_currentDotStepperIndex == 0) {
+                      return;
+                    }
+                    _currentDotStepperIndex--;
+                  });
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    if (_currentDotStepperIndex == _dotStepperAmount) {
+                      return;
+                    }
+                    _currentDotStepperIndex++;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      )
     );
   }
 }
