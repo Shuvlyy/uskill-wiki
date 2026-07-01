f31befa8bf1adf4c4ebfd2059a259b1dddbef718
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-01 17:07:58 +0200

    feat(app>widget>labeled_text_form_field): Added validator, maxLines and a mandatoriness indicator.

--- app/lib/widgets/labeled_text_form_field.dart
+++ app/lib/widgets/labeled_text_form_field.dart
@@ -1,12 +1,15 @@
 import 'package:app/core/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LabeledTextFormField extends StatelessWidget {
   final String label;
   final String hintText;
   final String initialValue;
   final void Function(String?) onSaved;
-  // is required, regex check...
+  final bool isRequired;
+  final String? Function(String?)? validator;
+  final int? maxLines;
 
   const LabeledTextFormField({
     super.key,
@@ -14,6 +17,9 @@ class LabeledTextFormField extends StatelessWidget {
     required this.hintText,
     required this.initialValue,
     required this.onSaved,
+    this.isRequired = true,
+    this.validator,
+    this.maxLines = 1
   });
 
   @override
@@ -22,18 +28,37 @@ class LabeledTextFormField extends StatelessWidget {
       crossAxisAlignment: .start,
       spacing: 5,
       children: [
-        Text(
-          label,
-          style: Theme.of(context).textTheme.labelLarge?.copyWith(
-            color: AppTheme.blackColor,
-            fontWeight: FontWeight.w600
-          ),
+        Row(
+          spacing: 2,
+          children: [
+            Text(
+              label,
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                  color: AppTheme.blackColor,
+                  fontWeight: FontWeight.w600
+              ),
+            ),
+            if (isRequired) ... {
+              SvgPicture.asset(
+                'pictos/star-of-life.svg',
+                height: 12,
+                color: AppTheme.secondaryRedColor,
+              )
+            }
+          ],
         ),
         TextFormField(
           initialValue: initialValue,
+          maxLines: maxLines,
           decoration: InputDecoration(
-            hintText: hintText
+            hintText: hintText,
           ),
+          validator: (val) {
+            if (isRequired && (val == null || val.isEmpty)) {
+              return "Veuillez remplir ce champ.";
+            }
+            return validator?.call(val);
+          },
           onSaved: onSaved,
         )
       ],
