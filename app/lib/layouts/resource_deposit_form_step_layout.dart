5eb70317968f2d1b1ceda2d207c729109d78a831
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-01 14:41:05 +0200

    feat(app>layout>resource_deposit_form_step_layout): Removed box background, added thick blue border and added a back button.

--- app/lib/layouts/resource_deposit_form_step_layout.dart
+++ app/lib/layouts/resource_deposit_form_step_layout.dart
@@ -1,8 +1,10 @@
 import 'package:app/core/theme.dart';
 import 'package:app/widgets/dot_stepper.dart';
 import 'package:app/widgets/icon_button.dart';
+import 'package:app/widgets/text_icon_button.dart';
 import 'package:app/widgets/title.dart';
 import 'package:flutter/material.dart' hide Title;
+import 'package:gap/gap.dart';
 
 class ResourceDepositFormStepLayout extends StatelessWidget {
   final String title;
@@ -18,34 +20,48 @@ class ResourceDepositFormStepLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: AppTheme.primaryColor,
-          width: 4
-        )
-      ),
-      // color: Color(0XFFCECECE),
-      padding: const EdgeInsets.all(40),
-      constraints: BoxConstraints(
-        maxWidth: 650
-      ),
-      child: Column(
-        crossAxisAlignment: .stretch,
-        spacing: 40,
-        children: [
-          Title(title: title),
-          body,
-          Row(
-            mainAxisAlignment: .spaceBetween,
-            spacing: 20,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: AppTheme.primaryColor,
+              width: 4
+            )
+          ),
+          padding: const EdgeInsets.all(40),
+          constraints: BoxConstraints(
+            maxWidth: 650 // todo: change?
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: 40,
             children: [
-              DotStepper(amount: 6, index: pageIndex),
-              PrimaryIconButton(icon: Icons.arrow_forward_ios, onPressed: () {})
+              Title(title: title),
+              body,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                spacing: 20,
+                children: [
+                  DotStepper(amount: 6, index: pageIndex),
+                  PrimaryIconButton(icon: Icons.arrow_forward_ios, onPressed: () {})
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        ),
+        const Gap(10),
+        TextIconButton(
+          icon: Icons.arrow_back_ios,
+          text: 'Back',
+          color: Colors.grey.shade500,
+          onTap: () {
+
+          }
+        ),
+      ],
     );
   }
 }
