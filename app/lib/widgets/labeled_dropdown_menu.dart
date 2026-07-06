417f7a3cf2dc0e0785390cae90fefb19805c3ab7
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-06 19:59:56 +0200

    feat(app>widget): Created `LabeledDropdownMenu` widget.

---
+++ app/lib/widgets/labeled_dropdown_menu.dart
@@ -0,0 +1,59 @@
+import 'package:app/core/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LabeledDropdownMenu<T> extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final List<DropdownMenuEntry<T>> dropdownMenuEntries;
+  final bool isRequired;
+  final T? initialSelection;
+  final void Function(T?) onSelected;
+
+  const LabeledDropdownMenu({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.dropdownMenuEntries,
+    this.isRequired = true,
+    this.initialSelection,
+    required this.onSelected
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: .start,
+      spacing: 5,
+      children: [
+        Row(
+          spacing: 2,
+          children: [
+            Text(
+              label,
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                  color: AppTheme.blackColor,
+                  fontWeight: FontWeight.w600
+              ),
+            ),
+            if (isRequired) ... {
+              SvgPicture.asset(
+                'pictos/star-of-life.svg',
+                height: 12,
+                color: AppTheme.secondaryRedColor,
+              )
+            }
+          ],
+        ),
+        DropdownMenu<T>(
+          hintText: hintText,
+          expandedInsets: EdgeInsets.zero,
+          requestFocusOnTap: false,
+          dropdownMenuEntries: dropdownMenuEntries,
+          initialSelection: initialSelection,
+          onSelected: onSelected,
+        )
+      ]
+    );
+  }
+}
