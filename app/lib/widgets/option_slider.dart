6cce2682916c2d58fea9fe8ebcd94b2f1bbeda95
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-06 20:00:18 +0200

    feat(app>widget>option_slider): Added `label` and `isRequired` parameters.

--- app/lib/widgets/option_slider.dart
+++ app/lib/widgets/option_slider.dart
@@ -1,13 +1,18 @@
 import 'package:app/core/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OptionSlider extends StatelessWidget {
+  final String label;
+  final bool isRequired;
   final List<String> steps;
   final int selectedIndex;
   final ValueChanged<int> onChanged;
 
   const OptionSlider({
     super.key,
+    required this.label,
+    this.isRequired = true,
     required this.steps,
     required this.selectedIndex,
     required this.onChanged,
@@ -18,26 +23,51 @@ class OptionSlider extends StatelessWidget {
     const double circleSize = 24.0;
     const double lineHeight = 6.0;
 
-    return Stack(
+    return Column(
+      crossAxisAlignment: .start,
+      spacing: 10,
       children: [
-        Positioned(
-          left: circleSize / 2,
-          right: circleSize / 2,
-          top: (circleSize / 2) - (lineHeight / 2),
-          child: Container(
-            height: lineHeight,
-            color: AppTheme.fieldOutlineColor,
-          ),
-        ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: List.generate(steps.length, (index) {
-            return _SliderNode(
-              label: steps[index],
-              isSelected: index == selectedIndex,
-              onTap: () => onChanged(index),
-            );
-          }),
+          spacing: 2,
+          children: [
+            Text(
+              label,
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                  color: AppTheme.blackColor,
+                  fontWeight: FontWeight.w600
+              ),
+            ),
+            if (isRequired) ... {
+              SvgPicture.asset(
+                'pictos/star-of-life.svg',
+                height: 12,
+                color: AppTheme.secondaryRedColor,
+              )
+            }
+          ],
+        ),
+        Stack(
+          children: [
+            Positioned(
+              left: circleSize / 2,
+              right: circleSize / 2,
+              top: (circleSize / 2) - (lineHeight / 2),
+              child: Container(
+                height: lineHeight,
+                color: AppTheme.fieldOutlineColor,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(steps.length, (index) {
+                return _SliderNode(
+                  label: steps[index],
+                  isSelected: index == selectedIndex,
+                  onTap: () => onChanged(index),
+                );
+              }),
+            ),
+          ],
         ),
       ],
     );
