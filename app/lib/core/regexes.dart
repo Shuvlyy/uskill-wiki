916913bf052d03647244590a42e59f498af8f2e1
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-07 17:06:55 +0200

    feat(app>core>regexes: Added URL regex.

--- app/lib/core/regexes.dart
+++ app/lib/core/regexes.dart
@@ -5,4 +5,9 @@ class Regexes {
     caseSensitive: false
   );
 
+  static final url = RegExp(
+    r'''https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)''',
+    caseSensitive: false
+  );
+
 }
