3e8c9b2cf0d346f0ab81f97ed9b23ea9d2951476
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-07 16:50:01 +0200

    feat(app>utils): Created Utils class ; created `launch` function to open a link in a new tab.

---
+++ app/lib/core/utils.dart
@@ -0,0 +1,12 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class Utils {
+
+  static Future<void> launch(String url, {bool isNewTab = true}) async {
+    await launchUrl(
+      Uri.parse(url),
+      webOnlyWindowName: isNewTab ? '_blank' : '_self',
+    );
+  }
+
+}
