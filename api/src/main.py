507fc4661e9deeac16ee696a7272f150edf08115
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-08 18:32:45 +0200

    fix(api): Added middleware to accept connections from flutter app.

--- api/src/main.py
+++ api/src/main.py
@@ -1,14 +1,25 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database import Base, engine
 from src.routers import resources
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)  # todo: use alembic for production
 
 app = fastapi.FastAPI(
     title="U-Skill Wiki API",
     description="",
     version="0.0.1",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],  # todo: !!! not for production. should be something like "allow_origins=["https://u-skill.univ-nantes.fr"]"
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
 )
 
 app.include_router(resources.router)
