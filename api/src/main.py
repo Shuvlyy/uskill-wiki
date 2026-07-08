68856741166351a24cefc4b685bcbb6744fbe63e
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-08 16:22:30 +0200

    feat(api): Connected main to API backend.

--- api/src/main.py
+++ api/src/main.py
@@ -1,10 +1,17 @@
 import fastapi
 
+from src.database import Base, engine
+from src.routers import resources
+
+Base.metadata.create_all(bind=engine)
+
 app = fastapi.FastAPI(
     title="U-Skill Wiki API",
     description="",
     version="0.0.1",
 )
+
+app.include_router(resources.router)
 
 
 @app.get("/ping")
