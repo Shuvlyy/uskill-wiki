95626cb95bd230f795f5147df862c022f4d94f8b
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-13 13:24:40 +0200

    feat(api>dependencies): Added admin verification.

--- api/src/dependencies.py
+++ api/src/dependencies.py
@@ -14,3 +14,12 @@ def get_db():
     finally:
         db.close()
 
+
+def verify_admin(x_admin_token: str = Header(...)):
+    """
+    Checks if the request includes the correct admin token in the headers.
+    If not, it instantly rejects the request with a 403 Forbidden error.
+    """
+    # todo: do jwt or ANYTHING more secure but this is for demo.
+    if x_admin_token != "demo":
+        raise HTTPException(status_code=403, detail="Not authorized as Admin")
