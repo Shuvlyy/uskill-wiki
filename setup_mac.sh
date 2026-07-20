0966e47a3282d9ad75f6e29c1558f95438e436bb
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-20 18:25:38 +0200

    fix(setup>mac): Fixed missing path export after installing homebrew.

--- setup_mac.sh
+++ setup_mac.sh
@@ -6,6 +6,9 @@ if ! command -v brew &> /dev/null
 then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo >> "$HOME/.zprofile"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 else
     echo "Homebrew is already installed ($(brew --version))."
 fi
