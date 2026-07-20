340157412311e2baadb31d9a1f32b058d4820323
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-20 17:22:47 +0200

    feat: Created script for automatic installation for Windows.

---
+++ setup_windows.bat
@@ -0,0 +1,37 @@
+@echo off
+echo U-Skill Wiki installation
+
+:: install python ::
+echo Installing Python...
+winget install -e --id Python.Python.3.11 --accept-package-agreements --accept-source-agreements
+
+:: install docker desktop ::
+echo Installing Docker Desktop (docker)...
+winget install -e --id Docker.DockerDesktop --accept-package-agreements --accept-source-agreements
+
+:: docker desktop startup ::
+echo Starting Docker Desktop...
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+echo Waiting for Docker service to spool up...
+:: todo: better way?? ::
+timeout /t 30 /nobreak >nul
+
+:: .env setup lol ::
+if not exist .env (
+    echo Please fill up the .env file ^(admin email + password^).
+    exit /b 0
+) else (
+    echo Environment variables are already set.
+)
+
+:: project startup!! ::
+echo Starting up U-Skill Wiki...
+docker compose up --build -d
+
+echo ==================================================
+echo Project has been started.
+echo App can be accessed on: http://localhost:8080
+echo And, if you are curious, API can be accessed on: http://localhost:8000
+echo ==================================================
+echo If you want to stop the project, simply type "docker compose down" in the console.
+pause >nul
