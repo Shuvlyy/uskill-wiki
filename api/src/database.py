8954896a4b0cab5424ac5174d29b714ecb53ab82
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-23 16:34:45 +0200

    fix: Fixed database being deleted after docker container down.

--- api/src/database.py
+++ api/src/database.py
@@ -1,7 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./uskill.db"
+os.makedirs("./data", exist_ok=True)
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/uskill.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
